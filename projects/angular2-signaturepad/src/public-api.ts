3e84720fe74c583168cb11b1b0000a4d16373ee9
Benoît <calvez@gmail.com>
2024-05-24 16:33:51 +0200

    Angular 17

--- projects/angular2-signaturepad/src/public-api.ts
+++ projects/angular2-signaturepad/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of angular2-signaturepad
  */
 
-export * from './lib/angular2-signaturepad.component';
-export * from './lib/angular2-signaturepad.module';
+export * from './lib/mo-angular2-signaturepad.component';
+export * from './lib/mo-angular2-signaturepad.module';
